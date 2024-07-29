853f80384a7e55be927f27e120a674eed0aba6cd
Nico Ponziani <nico.ponziani@dgsspa.com>
2024-07-29 11:09:45 +0200

    Reintroduzione del RestController per la root

--- src/main/java/it/pagopa/pn/pdfraster/PdfRasterApplication.java
+++ src/main/java/it/pagopa/pn/pdfraster/PdfRasterApplication.java
@@ -1,7 +1,8 @@
-package it.pagopa.pn.template;
+package it.pagopa.pn.pdfraster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,14 @@ public class PdfRasterApplication {
     public static void main(String[] args) {
         SpringApplication.run(PdfRasterApplication.class, args);
     }
-    
+
+    @RestController
+    @RequestMapping("/")
+    public static class RootController {
+
+        @GetMapping("/")
+        public String home() {
+            return HttpStatus.OK.name();
+        }
+    }
 }
