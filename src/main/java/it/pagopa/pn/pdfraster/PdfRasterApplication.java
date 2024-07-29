78d305c7dcdcac5998d8a342156a87a0aab121fd
NicoPonziani <84712226+NicoPonziani@users.noreply.github.com>
2024-07-29 11:29:50 +0200

    Feature/764 (#5)
    * Modifica Pom per adattamento template
    
    * cambio nome file OPENAPI , aggiunta properties base
    
    * rimozione classe di esempio per i test
    
    * Configurazione Open API
    
    * Fix Open API
    
    * Aggiunta template
    
    * [FIX] OpenAPI -> rimozione HealtCheck
    
    * Aggiunta dichiarazione del LogName sulle properties di Lombok
    
    * Reintroduzione del RestController per la root
    
    * [FIX] versione di pn-commons
    
    ---------
    
    Co-authored-by: Nico Ponziani <nico.ponziani@dgsspa.com>

--- src/main/java/it/pagopa/pn/pdfraster/PdfRasterApplication.java
+++ src/main/java/it/pagopa/pn/pdfraster/PdfRasterApplication.java
@@ -1,19 +1,19 @@
-package it.pagopa.pn.template;
+package it.pagopa.pn.pdfraster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class TemplateApplication {
+public class PdfRasterApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(TemplateApplication.class, args);
+        SpringApplication.run(PdfRasterApplication.class, args);
     }
-
 
     @RestController
     @RequestMapping("/")
@@ -21,7 +21,7 @@ public class TemplateApplication {
 
         @GetMapping("/")
         public String home() {
-            return "";
+            return HttpStatus.OK.name();
         }
     }
 }
