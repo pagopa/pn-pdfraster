96c6cad8bcfe60bccbfd997b9aa9921f198f1dc2
Nico Ponziani <nico.ponziani@dgsspa.com>
2024-08-01 14:59:27 +0200

    [767] Implementazione classe di implementazione per richiamare SafeStorage

--- src/main/java/it/pagopa/pn/pdfraster/rest/call/impl/SafeStorageCallImpl.java
+++ src/main/java/it/pagopa/pn/pdfraster/rest/call/impl/SafeStorageCallImpl.java
@@ -11,14 +11,32 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import static it.pagopa.pn.pdfraster.utils.LogUtils.POST_FILE;
+import static it.pagopa.pn.pdfraster.utils.LogUtils.SAFE_STORAGE_SERVICE;
+
 @CustomLog
 @Component
 public class SafeStorageCallImpl implements SafeStorageCall {
 
+    private final WebClient ssWebClient;
+    private final SafeStorageEndpointProperties safeStorageEndpointProperties;
+
+    public SafeStorageCallImpl(WebClient webClient, SafeStorageEndpointProperties safeStorageEndpointProperties){
+        this.ssWebClient = webClient;
+        this.safeStorageEndpointProperties = safeStorageEndpointProperties;
+    }
 
     @Override
     public Mono<FileCreationResponse> createFile(String xPagopaSafestorageCxId, String xApiKey, String checksumValue, String xTraceId, FileCreationRequest fileCreationRequest) {
-        return null;
+        log.logInvokingExternalService(SAFE_STORAGE_SERVICE, POST_FILE);
+        return ssWebClient.post().uri(safeStorageEndpointProperties.postFile())
+                .header(safeStorageEndpointProperties.clientHeaderName(), xPagopaSafestorageCxId)
+                .header(safeStorageEndpointProperties.apiKeyHeaderName(), xApiKey)
+                .header(safeStorageEndpointProperties.checksumValueHeaderName(), checksumValue)
+                .header(safeStorageEndpointProperties.traceIdHeaderName(), xTraceId)
+                .body(BodyInserters.fromValue(fileCreationRequest))
+                .retrieve()
+                .bodyToMono(FileCreationResponse.class);
     }
 
     @Override
