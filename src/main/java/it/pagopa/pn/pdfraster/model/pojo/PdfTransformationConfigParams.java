d2311fbd14ed99c5c6d6b83ebee5c7ae483969e5
CristianoZingaretti <cristianozingaretti@hotmail.it>
2024-08-07 14:59:07 +0200

    "Feature 784: Configuration class completed"

---
+++ src/main/java/it/pagopa/pn/pdfraster/model/pojo/PdfTransformationConfigParams.java
@@ -0,0 +1,20 @@
+package it.pagopa.pn.pdfraster.model.pojo;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@ToString(callSuper = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@SuperBuilder
+@NoArgsConstructor
+public class PdfTransformationConfigParams {
+    private String cropbox;
+    private Number dpi;
+    private String mediaSize;
+    private String margins;
+}
---
+++ src/main/java/it/pagopa/pn/pdfraster/utils/JsonUtils.java
@@ -0,0 +1,26 @@
+package it.pagopa.pn.pdfraster.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.pagopa.pn.pdfraster.exceptions.JsonStringToObjectException;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JsonUtils {
+
+    private final ObjectMapper objectMapper;
+
+    public JsonUtils(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public <T> T convertJsonStringToObject(String jsonString, Class<T> classToMap) throws JsonStringToObjectException {
+        try {
+            return objectMapper.readValue(jsonString, classToMap);
+        } catch (JsonProcessingException e) {
+            throw new JsonStringToObjectException(jsonString, classToMap);
+        }
+    }
+
+}
