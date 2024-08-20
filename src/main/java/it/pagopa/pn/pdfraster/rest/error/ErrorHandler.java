83b332b1adcda9231272ab07f38c59ca21b2ad03
Nico Ponziani <nico.ponziani@dgsspa.com>
2024-08-20 11:29:08 +0200

    [767] Modifica Error Handler per evitare che restituisca un Json

--- src/main/java/it/pagopa/pn/pdfraster/rest/error/ErrorHandler.java
+++ src/main/java/it/pagopa/pn/pdfraster/rest/error/ErrorHandler.java
@@ -1,14 +1,11 @@
 package it.pagopa.pn.pdfraster.rest.error;
 
-import it.pagopa.pn.common.rest.error.v1.dto.Problem;
 import it.pagopa.pn.pdfraster.exceptions.Generic400ErrorException;
 import it.pagopa.pn.pdfraster.exceptions.Generic500ErrorException;
 import lombok.CustomLog;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.UUID;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
@@ -18,24 +15,15 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 public class ErrorHandler {
 
     @ExceptionHandler(Generic400ErrorException.class)
-    public final ResponseEntity<Problem> handleBadRequest(Exception exception) {
-        var problem = new Problem();
-        problem.setStatus(BAD_REQUEST.value());
-        problem.setTitle("Bad request");
-        problem.setDetail("Malformed request");
-        problem.setTraceId(UUID.randomUUID().toString());
-        return new ResponseEntity<>(problem, BAD_REQUEST);
+    public final ResponseEntity<Void> handleBadRequest(Exception exception) {
+        log.warn("Bad Request", exception);
+        return ResponseEntity.status(BAD_REQUEST).build();
     }
 
-    @ExceptionHandler({Exception.class, Generic500ErrorException.class})
-    public final ResponseEntity<Problem> handleGenericError(Exception exception) {
-        var problem = new Problem();
-        problem.setStatus(INTERNAL_SERVER_ERROR.value());
-        problem.setTitle("Internal Server Error");
-        problem.setDetail(exception.getMessage());
-        problem.setTraceId(UUID.randomUUID().toString());
+    @ExceptionHandler({Generic500ErrorException.class,Exception.class})
+    public final ResponseEntity<Void> handleGenericError(Exception exception) {
         log.warn("Internal Server Error", exception);
-        return new ResponseEntity<>(problem, INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).build();
     }
 
 }
