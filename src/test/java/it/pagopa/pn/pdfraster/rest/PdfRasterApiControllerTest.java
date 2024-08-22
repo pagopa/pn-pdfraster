ad3f9cbeedffa1b601e4a2da809cf0a6f1a6169e
Nico Ponziani <nico.ponziani@dgsspa.com>
2024-08-22 17:16:58 +0200

    [768] Add Test KO PdfRasterApiControllerTest

--- src/test/java/it/pagopa/pn/pdfraster/rest/PdfRasterApiControllerTest.java
+++ src/test/java/it/pagopa/pn/pdfraster/rest/PdfRasterApiControllerTest.java
@@ -13,13 +13,14 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
 
 import static it.pagopa.pn.pdfraster.utils.TestUtils.getFileKoTestFromResources;
 import static it.pagopa.pn.pdfraster.utils.TestUtils.getFileTestFromResources;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.http.MediaType.APPLICATION_PDF;
+import static org.springframework.http.MediaType.*;
 
 @SpringBootTestWebEnv
 @AutoConfigureWebTestClient(timeout = "100000")
@@ -36,6 +37,7 @@ class PdfRasterApiControllerTest {
     private final static MultiValueMap<String, HttpEntity<?>> FILE_TEST_OK;
     private final static MultiValueMap<String, HttpEntity<?>> FILE_TEST_KO;
     private final static MultiValueMap<String, HttpEntity<?>> FILE_TEST_EMPTY;
+    private final static MultiValueMap<String, HttpEntity<?>> FILE_TEST_WRONG_CONTENT_TYPE;
 
     static{
         FILE = getFileTestFromResources();
@@ -43,7 +45,7 @@ class PdfRasterApiControllerTest {
         FILE_TEST_OK = getMultipartFileTest(FILE);
         FILE_TEST_KO = getMultipartFileTest(FILE_KO);
         FILE_TEST_EMPTY = getMultipartFileTest(new byte[0]);
-
+        FILE_TEST_WRONG_CONTENT_TYPE = getMultipartFileTestWrongContentType(FILE);
     }
 
     /**
@@ -59,12 +61,29 @@ class PdfRasterApiControllerTest {
                     public String getFilename() {
                         return "TEST.pdf";
                     }
-                }).header(HttpHeaders.CONTENT_DISPOSITION, "form-data; name=file; filename=TEST.pdf")
-                .contentType(APPLICATION_PDF);
+                });
 
         return builder.build();
     }
 
+    /**
+     * Metodo per creare un MultipartFile contente il file di test
+     *
+     * @return
+     */
+    public static MultiValueMap<String, HttpEntity<?>> getMultipartFileTestWrongContentType(byte[] file){
+        MultipartBodyBuilder builder = new MultipartBodyBuilder();
+
+        builder.part("file", new ByteArrayResource(file) {
+                    @Override
+                    public String getFilename() {
+                        return "TEST.pdf";
+                    }
+                }).header(HttpHeaders.CONTENT_DISPOSITION, "form-data; name=file; filename=TEST.pdf;content-type=application/json")
+                .contentType(APPLICATION_JSON);
+
+        return builder.build();
+    }
     /**
      *
      * @return
@@ -72,6 +91,38 @@ class PdfRasterApiControllerTest {
     private @NotNull WebTestClient.ResponseSpec getResponseEntityExchangeResult(MultiValueMap<String, HttpEntity<?>> body) {
         return webTestClient.post()
                 .uri(uriBuilder -> uriBuilder.path(CONVERT_PDF_PATH).build())
+                .contentType(MULTIPART_FORM_DATA)
+                .accept(APPLICATION_PDF)
+                .body(BodyInserters.fromMultipartData(body))
+                .exchange();
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    private @NotNull WebTestClient.ResponseSpec getResponseEntityExchangeResultMissingBody() {
+        return webTestClient.post()
+                .uri(uriBuilder -> uriBuilder.path(CONVERT_PDF_PATH).build())
+                .contentType(MULTIPART_FORM_DATA)
+                .accept(APPLICATION_PDF)
+                .exchange();
+    }
+
+    /**
+     *
+     * @param body
+     * @return
+     */
+    private @NotNull WebTestClient.ResponseSpec getResponseEntityExchangeResultWrongContentType(MultiValueMap<String, HttpEntity<?>> body) {
+        MultiValueMap<String,String> headers = new LinkedMultiValueMap<>();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "application/json; name=\"file\"; filename=\"TEST.pdf\"");
+        headers.add(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE);
+        return webTestClient.post()
+                .uri(uriBuilder -> uriBuilder.path(CONVERT_PDF_PATH).build())
+                .headers(httpHeaders ->  httpHeaders.addAll(headers))
+                .contentType(APPLICATION_JSON)
                 .accept(APPLICATION_PDF)
                 .body(BodyInserters.fromMultipartData(body))
                 .exchange();
@@ -103,6 +154,36 @@ class PdfRasterApiControllerTest {
     @Test
     void convertPdf_EmptyFile(){
         getResponseEntityExchangeResult(FILE_TEST_EMPTY)
+                .expectStatus().isBadRequest()
+                .expectBody(Resource.class)
+                .consumeWith(ex ->{
+                    Assertions.assertNull(ex.getResponseBody());
+                });
+    }
+
+//    @Test
+//    void convertPdf_ContentType_KO(){
+//        getResponseEntityExchangeResultWrongContentType(FILE_TEST_OK)
+//                .expectStatus().isBadRequest()
+//                .expectBody(Resource.class)
+//                .consumeWith(ex ->{
+//                    Assertions.assertNull(ex.getResponseBody());
+//                });
+//    }
+
+    @Test
+    void convertPdf_ContentTypeMultipart_KO(){
+        getResponseEntityExchangeResultWrongContentType(FILE_TEST_WRONG_CONTENT_TYPE)
+                .expectStatus().isBadRequest()
+                .expectBody(Resource.class)
+                .consumeWith(ex ->{
+                    Assertions.assertNull(ex.getResponseBody());
+                });
+    }
+
+    @Test
+    void convertPdf_MissingRequest_KO(){
+        getResponseEntityExchangeResultMissingBody()
                 .expectStatus().isBadRequest()
                 .expectBody(Resource.class)
                 .consumeWith(ex ->{
