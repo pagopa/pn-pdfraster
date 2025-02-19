d466fa6e92f03860a8d9904c6aa1e3fff1dcb09c
michelescara <michele.scaramuzzino@gmail.com>
2025-02-19 10:57:11 +0100

    PN-13817: Added MDCWebFilterActivation for MDC context propagation. Added variable reference for accountID in storage.yml

---
+++ src/main/java/it/pagopa/pn/pdfraster/configuration/MDCWebFilterActivation.java
@@ -0,0 +1,8 @@
+package it.pagopa.pn.pdfraster.configuration;
+
+import it.pagopa.pn.commons.log.MDCWebFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MDCWebFilterActivation extends MDCWebFilter {
+}
