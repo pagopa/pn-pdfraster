e0f61e49993c35c34b862b61b7fd5e930fe858da
michelescara <79908782+michelescara@users.noreply.github.com>
2025-03-17 14:29:21 +0100

    Feature/pn 13373 (#60)
    * PN-13373: Introduced init.sh script for localstack.
    
    * add local-properties and start.sh
    
    * PN-13373: Fixed localstack endpoint and server port.
    
    * fix log init.sh
    
    * PN-13373: Changed default port in application-local.properties.
    
    * PN-13373: fixed LOCALSTACK_ENDPOINT in init.sh
    
    * PN-13373: Fixed pn-PDFRaster parameter value in init.sh script.
    
    * [1.2.0-RC.1] Modifica versione nel pom.xml
    
    * [1.2.0-RC.2] Modifica versione nel pom.xml
    
    * [1.2.0] Create Release Version
    
    * PN-14048 - bump pdfbox version 3.0.4
    
    * PN-14048 - added dependency for jbig2-imageio
    
    * PN-14048: bump version
    
    * [1.2.1-RC.1] Modifica versione nel pom.xml
    
    * [1.2.1] Create Release Version
    
    * 1.3.0-SNAPSHOT
    
    * PN-13373: Added missing configurations for init.sh.
    
    ---------
    
    Co-authored-by: MartinaMattei <martinamattei.mm00@gmail.com>
    Co-authored-by: mottone <mottone@email.it>
    Co-authored-by: GitHub Actions Bot <>
    Co-authored-by: damiano.lozzi <damiano.lozzi@dgsspa.com>
    Co-authored-by: mottone-dgs <mario.ottone@dgsspa.com>
    Co-authored-by: Marco Iannaccone <marco.iannaccone@pagopa.it>

---
+++ scripts/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+curr_dir=$(pwd)
+echo "### Starting pn-pdfraster ###"
+
+if ! ../src/test/resources/testcontainers/init.sh; then
+  echo "### Failed to run init.sh ###"
+  exit 1
+fi
+
+cd ..
+
+if ! ./mvnw -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=local -Daws.accessKeyId=TEST -Daws.secretAccessKey=TEST -Daws.region=eu-south-1" spring-boot:run; then
+  echo "### Initialization failed ###"
+  exit 1
+fi
+
+# Return to the original directory
+cd "$curr_dir"
