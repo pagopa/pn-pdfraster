685547e913710d3a1df97b283dc6d2d9de610198
Martina Mattei <82142676+martiMattei@users.noreply.github.com>
2025-03-18 17:15:01 +0100

    Feature/pn 14192 (#61)
    * PN-14192: Add SpringAnalyzer for pn-pdfraster
    
    * PN-14192: Add implementation for SpringAnalyzer
    
    * PN-14192: Add getTaskId for ECS
    
    * PN-14192: Add properties for spring
    
    * PN-14192: modified property for spring
    
    * PN-14192: modified SpringAnalyzerActivation
    
    * PN-14192: modified property for chron
    
    * PN-14192: modified pn-commons version
    
    * PN-14192: modified properties for getting parameters from pn-commons

--- src/main/java/it/pagopa/pn/pdfraster/configuration/AwsConfiguration.java
+++ src/main/java/it/pagopa/pn/pdfraster/configuration/AwsConfiguration.java
@@ -1,17 +1,24 @@
 package it.pagopa.pn.pdfraster.configuration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.messaging.config.QueueMessageHandlerFactory;
 import io.awspring.cloud.messaging.listener.support.AcknowledgmentHandlerMethodArgumentResolver;
 import it.pagopa.pn.pdfraster.configuration.properties.AwsConfigurationProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
+import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClientBuilder;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.S3AsyncClientBuilder;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
@@ -22,6 +29,7 @@ import software.amazon.awssdk.services.ssm.SsmClientBuilder;
 import java.net.URI;
 import java.util.List;
 
+@Slf4j
 @Configuration
 public class AwsConfiguration {
 
@@ -36,7 +44,12 @@ public class AwsConfiguration {
     @Value("${test.aws.s3.endpoint:#{null}}")
     private String testAwsS3Endpoint;
 
+    @Value("${test.aws.cloudwatch.endpoint:#{null}}")
+    private String testAwsCloudwatchEndpoint;
+
     private static final DefaultCredentialsProvider DEFAULT_CREDENTIALS_PROVIDER_V2 = DefaultCredentialsProvider.create();
+    private final WebClient genericWebClient = WebClient.builder().build();
+
 
     public AwsConfiguration(AwsConfigurationProperties awsConfigurationProperties) {
         this.awsConfigurationProperties = awsConfigurationProperties;
@@ -96,6 +109,51 @@ public class AwsConfiguration {
                 new PayloadMethodArgumentResolver(converter, validator)));
 
         return queueMessageHandlerFactory;
+    }
+
+    @Bean
+    public CloudWatchAsyncClient cloudWatchAsyncClient() {
+        CloudWatchAsyncClientBuilder cloudWatchAsyncClientBuilder = CloudWatchAsyncClient.builder().credentialsProvider(DEFAULT_CREDENTIALS_PROVIDER_V2).region(Region.of(awsConfigurationProperties.regionCode()));
+
+        if (testAwsCloudwatchEndpoint != null) {
+            cloudWatchAsyncClientBuilder.endpointOverride(URI.create(testAwsCloudwatchEndpoint));
+        }
+
+        return cloudWatchAsyncClientBuilder.build();
+    }
+
+
+    private String getTaskId() {
+        String ecsMetadataUri = System.getenv("ECS_CONTAINER_METADATA_URI_V4");
+        if (ecsMetadataUri == null) {
+            log.error("ECS_CONTAINER_METADATA_URI_V4 environment variable not found.");
+            return "streams-worker";
+        }
+
+        return genericWebClient.get()
+                .retrieve()
+                .bodyToMono(String.class)
+                .flatMap(response -> {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    try {
+                        JsonNode jsonNode = objectMapper.readTree(response);
+                        String taskArn = jsonNode.get("TaskARN").asText();
+                        String[] parts = taskArn.split("/");
+                        if (parts.length > 0) {
+                            return Mono.just(parts[parts.length - 1]);
+                        } else {
+                            log.error("Invalid TaskARN format");
+                            return Mono.just("streams-worker");
+                        }
+                    } catch (JsonProcessingException e) {
+                        log.error("Error while parsing JSON response", e);
+                        return Mono.just("streams-worker");
+                    }
+                })
+                .onErrorResume(throwable -> {
+                    log.error("Error while fetching container metadata", throwable);
+                    return Mono.just("streams-worker");
+                }).block();
     }
 
 }
