1f6eadc4fe384e41982992c5c08be9d650557bca
MartinaMattei <martinamattei.mm00@gmail.com>
2025-03-18 11:43:55 +0100

    PN-14192: modified property for chron

--- src/main/java/it/pagopa/pn/pdfraster/configuration/springboot/SpringAnalyzerActivation.java
+++ src/main/java/it/pagopa/pn/pdfraster/configuration/springboot/SpringAnalyzerActivation.java
@@ -6,7 +6,6 @@ import it.pagopa.pn.commons.utils.metrics.cloudwatch.CloudWatchMetricHandler;
 import lombok.CustomLog;
 import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,40 +22,6 @@ public class SpringAnalyzerActivation extends SpringAnalyzer {
         this.meterRegistry = meterRegistry;
     }
 
-    // Override del metodo scheduledSendMetrics con logica di retry per gestire il caso in cui il client non riesce ad eseguire la richiesta perchè il thread si è chiuso
-    @Override
-    @Scheduled(cron = "${pn.analyzer.cloudwatch-metric-cron}")
-    public void scheduledSendMetrics() {
-        sendMetricsWithRetry(3, 1000); // 3 tentativi, 1 secondo di intervallo
-    }
-
-    public void sendMetricsWithRetry(int retries, long delayMillis) {
-        int attempts = 0;
-        while (attempts < retries && !isExecutorActive()) {
-            try {
-                log.info("Attempting to send metrics... Attempt #" + (attempts + 1));
-                super.scheduledSendMetrics();  // chiama il metodo originale per inviare le metriche
-                break;
-            } catch (Exception e) {
-                attempts++;
-                log.error("Error sending metrics on attempt #" + attempts, e);
-                if (attempts >= retries) {
-                    log.error("Max retries reached. Metrics not sent.");
-                }
-                try {
-                    Thread.sleep(delayMillis);
-                } catch (InterruptedException ie) {
-                    Thread.currentThread().interrupt();
-                    break;
-                }
-            }
-        }
-    }
-
-    private boolean isExecutorActive() {
-        // check se l'executor è ancora su
-        return !Thread.currentThread().isInterrupted();
-    }
 }
 
 
