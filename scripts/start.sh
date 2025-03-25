85cdd7f3e924ccbf8026cc955304388470ac5142
michelescara <michele.scaramuzzino@gmail.com>
2025-03-25 16:54:35 +0100

    PN-13373: Removed init.sh invocation in start.sh script.

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,12 +2,6 @@
 
 curr_dir=$(pwd)
 echo "### Starting pn-pdfraster ###"
-
-if ! ../src/test/resources/testcontainers/init.sh; then
-  echo "### Failed to run init.sh ###"
-  exit 1
-fi
-
 cd ..
 
 if ! ./mvnw -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=local -Daws.accessKeyId=TEST -Daws.secretAccessKey=TEST -Daws.region=eu-south-1" spring-boot:run; then
