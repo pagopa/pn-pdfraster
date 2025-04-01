369ce3df91111c39d86dd456794fe9e2c50cf455
Martina Mattei <82142676+martiMattei@users.noreply.github.com>
2025-04-01 17:22:55 +0200

    Feature/tech bing25.2 (#65)
    * PN-14149 - bump pn-commons to 2.7.0 (latest) (#58)
    
    * PN-14149 - bump pn-commons 2.8.0
    
    * Feature/pn 13373 (#60)
    
    * PN-13373: Introduced init.sh script for localstack.
    
    * add local-properties and start.sh
    
    * PN-13373: Fixed localstack endpoint and server port.
    
    * fix log init.sh
    
    * PN-13373: Changed default port in application-local.properties.
    
    * PN-13373: fixed LOCALSTACK_ENDPOINT in init.sh
    
    * PN-13373: Fixed pn-PDFRaster parameter value in init.sh script.
    
    * [1.2.0-RC.1] Modifica versione nel pom.xml
    
    * [1.2.0-RC.2] Modifica versione nel pom.xml
    
    * [1.2.0] Create Release Version
    
    * PN-14048 - bump pdfbox version 3.0.4
    
    * PN-14048 - added dependency for jbig2-imageio
    
    * PN-14048: bump version
    
    * [1.2.1-RC.1] Modifica versione nel pom.xml
    
    * [1.2.1] Create Release Version
    
    * 1.3.0-SNAPSHOT
    
    * PN-13373: Added missing configurations for init.sh.
    
    ---------
    
    Co-authored-by: MartinaMattei <martinamattei.mm00@gmail.com>
    Co-authored-by: mottone <mottone@email.it>
    Co-authored-by: GitHub Actions Bot <>
    Co-authored-by: damiano.lozzi <damiano.lozzi@dgsspa.com>
    Co-authored-by: mottone-dgs <mario.ottone@dgsspa.com>
    Co-authored-by: Marco Iannaccone <marco.iannaccone@pagopa.it>
    
    * Feature/pn 14192 (#61)
    
    * PN-14192: Add SpringAnalyzer for pn-pdfraster
    
    * PN-14192: Add implementation for SpringAnalyzer
    
    * PN-14192: Add getTaskId for ECS
    
    * PN-14192: Add properties for spring
    
    * PN-14192: modified property for spring
    
    * PN-14192: modified SpringAnalyzerActivation
    
    * PN-14192: modified property for chron
    
    * PN-14192: modified pn-commons version
    
    * PN-14192: modified properties for getting parameters from pn-commons
    
    * PN-14290 - fix sonarcloud issues (#62)
    
    * PN-14290 - fix sonarcloud issues
    
    * PN-14290 - fix sonarcloud issues
    
    * PN-14290-bis - fix sonarcloud issues (#63)
    
    * PN-13373: Removed init.sh invocation in start.sh script. (#64)
    
    * TECH-BING25.2: changed PnCronAnalyzer value
    
    ---------
    
    Co-authored-by: Riccardo Pompili <131166836+RiccardoPompili@users.noreply.github.com>
    Co-authored-by: Riccardo Pompili <riccardo.pompili@dgsspa.com>
    Co-authored-by: michelescara <michele.scaramuzzino@gmail.com>
    Co-authored-by: michelescara <79908782+michelescara@users.noreply.github.com>
    Co-authored-by: mottone <mottone@email.it>
    Co-authored-by: damiano.lozzi <damiano.lozzi@dgsspa.com>
    Co-authored-by: mottone-dgs <mario.ottone@dgsspa.com>
    Co-authored-by: Marco Iannaccone <marco.iannaccone@pagopa.it>

---
+++ src/test/java/it/pagopa/pn/pdfraster/utils/FontUtilsTest.java
@@ -0,0 +1,70 @@
+package it.pagopa.pn.pdfraster.utils;
+
+import it.pagopa.pn.pdfraster.utils.annotation.SpringBootTestWebEnv;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTestWebEnv
+class FontUtilsTest {
+
+    @TempDir
+    Path tempDir;
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("user.home", tempDir.toString());
+    }
+
+    @Test
+    public void testLoadFonts() throws IOException {
+        FontUtils.loadFonts();
+
+        Path fontDir = tempDir.resolve(".fonts");
+        assertTrue(Files.exists(fontDir) && Files.isDirectory(fontDir), "La cartella dei font deve esistere");
+
+        Path copiedFont = fontDir.resolve("arial.ttf");
+        assertTrue(Files.exists(copiedFont), "Il font 'arial.ttf' deve essere stato copiato");
+
+        try (InputStream originalStream = getClass().getResourceAsStream("/fonts/arial.ttf")) {
+            assertNotNull(originalStream, "La risorsa originale 'arial.ttf' deve esistere nei test resources");
+            byte[] originalBytes = originalStream.readAllBytes();
+            byte[] copiedBytes = Files.readAllBytes(copiedFont);
+            assertArrayEquals(originalBytes, copiedBytes, "Il contenuto del font copiato deve essere uguale a quello della risorsa originale");
+        }
+    }
+
+
+    @Test
+    public void testLoadFonts_directoryCreationFails() {
+        File mockFile = mock(File.class);
+        when(mockFile.exists()).thenReturn(false);
+        when(mockFile.mkdirs()).thenReturn(false); // Simuliamo il fallimento della creazione della cartella
+
+        System.setProperty("user.home", "/mock/home");
+
+        PathMatchingResourcePatternResolver mockResolver = mock(PathMatchingResourcePatternResolver.class);
+        Resource[] mockResources = new Resource[0]; // Nessuna risorsa
+
+        try {
+            when(mockResolver.getResources("classpath:fonts/*.ttf")).thenReturn(mockResources);
+
+            FontUtils.loadFonts(); // Metodo statico da testare
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
